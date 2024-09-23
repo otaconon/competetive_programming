0c9cbf50bf02350ce4b0a901dd2be5558cebe3b4
Nignik <maskymilian.olbrys@gmail.com>
2024-09-23 18:49:34 +0200

    daily

--- src/KthLargestConnectedComponent.cpp
+++ src/KthLargestConnectedComponent.cpp
@@ -2,12 +2,79 @@
 
 using namespace std;
 
+class DSU {
+public:
+    DSU(vector<int>&& vec)
+        : up(vec)
+    {
+        mx.resize(up.size());
+        for (int i = 0; i < up.size(); i++) {
+            mx[i].push_back(i);
+        }
+        members.resize(up.size(), 1);
+    }
+
+    int find(int u) {
+        if (up[u] == u)
+            return u;
+        return up[u] = find(up[u]);
+    }
+
+    void unify(int u, int v) {
+        u = find(u);
+        v = find(v);
+
+        if (u == v)
+            return;
+
+        if (members[u] < members[v])
+            swap(u, v);
+
+        mx[u].insert(mx[u].end(), mx[v].begin(), mx[v].end());
+        sort(mx[u].rbegin(), mx[u].rend());
+        mx[u] = vector<int>(mx[u].begin(), mx[u].begin()+min(10, (int)mx[u].size()));
+
+        members[u] += members[v];
+        up[v] = u;
+    }
+
+    int getMx(int u, int k) {
+        u = find(u);
+        if (mx[u].size() < k)
+            return -1;
+
+        return mx[u][k-1] + 1;
+    }
+
+private:
+    vector<int> up;
+    vector<int> members;
+
+    vector<vector<int>> mx;
+};
+
 int main() {
     int n, q;
     cin >> n >> q;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        a[i] = i;
+    }
+
+    DSU dsu(move(a));
     
     while (q--) {
         int x, u, v;
         cin >> x >> u >> v;
+        u--;
+
+        if (x == 1) {
+            v--;
+            dsu.unify(u, v);
+        }
+        else {
+            cout << dsu.getMx(u, v) << endl;
+        }
     }
 }
--- src/test.cpp
+++ src/test.cpp
@@ -5,19 +5,47 @@ using namespace std;
 using Fp = atcoder::modint998244353;
 using i64 = long long;
 
-int main() {
-    i64 n;
-    int k;
-    cin >> n >> k;
+const double h = 6.62607015e-34;
+const double c = 2.99792458e8;
+const double k = 1.380649e-23;
 
-    const Fp p = Fp(2 * (n - 1)) / Fp(n).pow(2);
-    const Fp q = Fp(2) / Fp(n).pow(2);
-    Fp d = 1;
-    for (int i = 0; i < k; ++i) {
-        d = (1 - q - p) * d + q;
+double plancksLaw(double lambda_nm, double T) {
+    double lambda_m = lambda_nm * 1e-9;
+
+    double exponent = h * c / (lambda_m * k * T);
+
+    if (exponent > 700) {
+        return 0.0;
     }
 
-    const Fp u = Fp(n + 2) / 2;
-    const Fp h = (1 - d);
-    cout << (d + u * h).val() << endl;
+    double expTerm = exp(exponent) - 1.0;
+
+    if (expTerm == 0.0) {
+        return 0.0;
+    }
+
+    double numerator = 2.0 * h * c * c;
+    double denominator = pow(lambda_m, 5.0) * expTerm;
+    double B_lambda = numerator / denominator;
+
+    return B_lambda;
+}
+
+double f(double lambda_nm, double T) {
+    double lambda_m = lambda_nm * 1e-9;
+    return ((2 * h * c * c) / (pow(lambda_m, 5))) * (1 / (exp((h * c) / (lambda_m * k * T)) - 1));
+}
+
+int main() {
+    double lambda_nm = 500.0;  // Example wavelength in nm
+    double T = 6000.0;         // Example temperature in Kelvin
+
+    cin >> lambda_nm >> T;
+
+    double radiance = plancksLaw(lambda_nm, T);
+
+    std::cout << "Spectral radiance at " << lambda_nm << " nm and " << T << " K is " << radiance << " W·sr⁻¹·m⁻³" << std::endl;
+    std::cout << "Spectral radiance at " << lambda_nm << " nm and " << T << " K is " << f(lambda_nm, T) << " W·sr⁻¹·m⁻³" << std::endl;
+
+    return 0;
 }
