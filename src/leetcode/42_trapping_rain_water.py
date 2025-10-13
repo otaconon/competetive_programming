285224e997e736b85989a50fddc5b90ae32e714f
Nignik <maksymilian.olbrys@gmail.com>
2025-10-13 13:08:19 +0200

    leetcode: 42 trapping rain water

---
+++ src/leetcode/42_trapping_rain_water.py
@@ -0,0 +1,21 @@
+class Solution:
+  def trap(self, height: list[int]) -> int:
+    n = len(height)
+    ans = 0
+    
+    max_right = [0] * n
+    max_left = [0] * n
+    for i in range(1, n):
+      max_left[i] = max(max_left[i-1], height[i-1])
+    for i in range(n-2, 0, -1):
+      max_right[i] = max(max_right[i+1], height[i+1])
+      
+    for i in range(n):
+      ans += max(0, min(max_left[i], max_right[i]) - height[i])
+        
+    return ans 
+  
+sol = Solution()
+print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+print(sol.trap([4,2,0,3,2,5]))
+print(sol.trap([4, 2, 3]))
